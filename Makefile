d0e154c2b5e1c65a767a85e229eb1a8ec37bbf49
raamydib@gmail.com <radib@fu-r6-p2.s19.be>
2025-05-06 16:05:32 +0200

    rien vide nada

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME = 
+SOURCES = 
+
+
+
+OBJECTS = $(SOURCES:.c=.o)
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+
+all: $(NAME)
+
+$(NAME): $(OBJECTS)
+	$(AR) -r $@ $?
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) $?
+
+clean:
+	rm -f $(OBJECTS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all bonus clean fclean re
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 15:01:40 by radib             #+#    #+#             */
+/*   Updated: 2025/05/06 15:32:31 by radib            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdio.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
