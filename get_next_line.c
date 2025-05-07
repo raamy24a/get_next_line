b39d1ed0395daef941274c38c5981b0d24ff866c
Raamy Dib <radib@student.s19.be>
2025-05-07 17:27:22 +0200

    je n'ai pas fais assez de travail mais bon vaut mieux push

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,32 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 15:31:04 by radib             #+#    #+#             */
-/*   Updated: 2025/05/06 15:33:58 by radib            ###   ########.fr       */
+/*   Updated: 2025/05/07 17:26:17 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
+#include "unistd.h"
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+#endif
+
+char	*get_next_line(int fd)
 {
-	
+	size_t		nbr_read;
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*ptr;
+
+
+	nbr_read = read(fd, buffer, BUFFER_SIZE);
+	if (nbr_read == -1 || nbr_read == 0)
+		return (NULL);
+	if (buffer && nbr_read > 0)
+		buffer[nbr_read] = '\0';
+	ptr = buffer;
+	while (*ptr++ != '\n' && *ptr++)
+		{
+			if (*ptr == '\n')
+				return (full_line);
+		}
 }
