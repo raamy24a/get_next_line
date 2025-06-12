080c8ed0f202630ec5e8874868a4ab3b7828cc8c
raamydib@gmail.com <radib@shi-r5-p3.s19.be>
2025-06-12 13:00:25 +0200

    removed the main and a useless include

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 15:01:40 by radib             #+#    #+#             */
-/*   Updated: 2025/06/12 12:33:06 by radib            ###   ########.fr       */
+/*   Updated: 2025/06/12 12:45:12 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@
 size_t	ft_pstrlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
-char	*ft_strrchr_and_free(char *end_buffer, char mode);
 char	*ft_strrchr_a(char *s, char *new_stash);
 char	*ft_strrchr_b(char *s);
 char	*ft_strdup(const char *s);
